147a7cc4c6a53bc2de55beb39ab995ab4661f556
legisetiawan <legi.setiawan@gmail.com>
2022-07-20 16:05:15 +0700

    mengimpor component mui dan Mengatur component

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import './App.css';
 import Home from './containers/Home';
 import Login from './containers/Login';
+import Register from './containers/Register';
 
 function App() {
   return (
     <>
     <Login/>
-    <Home/>  
+    <Home/>
+    <Register/>
     </>
   );
 }
