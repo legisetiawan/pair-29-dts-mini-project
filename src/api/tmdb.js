71d98fd10ef44ecadb9de16b48af8e01e3d2843f
legi setiawan <61065053+legisetiawan@users.noreply.github.com>
2022-07-20 20:54:08 +0700

    Update tmdb.js

--- src/api/tmdb.js
+++ src/api/tmdb.js
@@ -2,7 +2,7 @@ import axios from "axios";
 const tmdbInstance = axios.create({
     baseURL:"https://api.themoviedb.org/3",
     params:{
-        api_key:"ab7237785cf23a0506b6a556bda68e60",
+        api_key:"api_key",
     },
 });
 export default tmdbInstance;
