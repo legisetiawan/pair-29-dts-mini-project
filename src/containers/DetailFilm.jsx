b3b73e9ae7469ef425837f4e2931f05255c7d387
legisetiawan <legi.setiawan@gmail.com>
2022-07-20 19:18:42 +0700

    Mengimpor tmdb api, membuat usestate,useeffect untuk mengambil data dari api

--- src/containers/DetailFilm.jsx
+++ src/containers/DetailFilm.jsx
@@ -1,33 +1,45 @@
-import React,{useState,useEffect} from 'react'
-import { Box,Rating } from '@mui/material';
-import Card from '@mui/material/Card';
-import tmdbInstance from '../api/tmdb';
-import CardMovie from '../components/CardMovie';
-import CardMedia from '@mui/material/CardMedia';
-import CardContent from '@mui/material/CardContent';
-import Typography from '@mui/material/Typography';
-
+import React, { useState, useEffect } from "react";
+import { Box, Rating } from "@mui/material";
+import Card from "@mui/material/Card";
+import tmdb from "../api/tmdb";
+import CardMovie from "../components/CardMovie";
+import CardMedia from "@mui/material/CardMedia";
+import CardContent from "@mui/material/CardContent";
+import Typography from "@mui/material/Typography";
 
 const DetailFilm = () => {
-    const [movies,setMovies] = useState([]);
-    
+  const [movies, setMovies] = useState([]);
+  useEffect(() => {
+    const fetchDataMovies = async () => {
+      try {
+        // Gunakan instance tmdb di sini
+        const responseDariTMDB = await tmdb.get(
+          // Nah di sini kita tidak perlu menuliskan terlalu panjang lagi
+          "/movie/popular"
+        );
+
+        // Jangan lupa set statenya
+        // Perhatikan di sini responseDariTMDB ada .data (response schema axios)
+        setMovies(responseDariTMDB.data.results);
+      } catch (err) {
+        console.log(err);
+      }
+    };
+
+    fetchDataMovies();
+  }, []);
+
   return (
     <>
-    <Card>
-        <Box className="boxy">
-          <Typography variant="h5"> Movie</Typography>
-        </Box>
-        <Box className="Boxy" sx={{ display: "flex", flexDirection: "row", alignItems: "center" }}>
-          <CardMedia component="img"  alt="kucing" sx={{ width: 150 }}></CardMedia>
-          <CardContent>
-            <Typography variant="body1"></Typography>
-            <Rating precision={0.1} readOnly />
-            <Typography variant="body1"></Typography>
-          </CardContent>
-        </Box>
-      </Card>
+      <Box>
+        <Typography variant="h5">List Movies Populer TMDB</Typography>
+
+        {movies.map((movie) => {
+          return <CardMovie movie={movie} />;
+        })}
+      </Box>
     </>
   );
-}
+};
 
-export default DetailFilm
+export default DetailFilm;
