5ff52526a3bb177b6531681d8f01f468fe47a4a5
legisetiawan <legi.setiawan@gmail.com>
2022-07-22 00:38:43 +0700

    Memperbaiki form loginOrregister

--- src/components/LoginOrRegisterForm.jsx
+++ src/components/LoginOrRegisterForm.jsx
@@ -1,16 +1,11 @@
 import React, { useEffect, useState } from "react";
 
-import styles from "./LoginOrRegisterForm.module.css";
-
-import { Grid, Box, Button, TextField, Typography } from "@mui/material";
+import { Box, Button, TextField, Typography,Avatar,CssBaseline,Container } from "@mui/material";
+import Face from '@mui/icons-material/Face'
 import { Link } from "react-router-dom";
 
 import { useNavigate } from "react-router-dom";
-import {
-  auth,
-  loginDenganEmailDanPassword,
-  registerDenganEmailDanPassword,
-} from "../authentication/firebase";
+import { auth, loginDenganEmailDanPassword, registerDenganEmailDanPassword } from "../authentication/firebase";
 
 import { useAuthState } from "react-firebase-hooks/auth";
 
@@ -42,7 +37,6 @@ const LoginOrRegisterForm = ({ loginOrRegister }) => {
   };
 
   const registerHandler = () => {
-  
     registerDenganEmailDanPassword(credential.email, credential.password);
   };
 
@@ -53,63 +47,37 @@ const LoginOrRegisterForm = ({ loginOrRegister }) => {
       registerHandler();
     }
   };
-  useEffect(
-    () => {
-      if (isLoading) {
-        return;
-      }
+  useEffect(() => {
+    if (isLoading) {
+      return;
+    }
 
-    
-      if (user) {
-        navigate("/");
-      }
-    },
-    [user, isLoading, navigate]
-  );
+    if (user) {
+      navigate("/");
+    }
+  }, [user, isLoading, navigate]);
 
   return (
-    <Grid
-      container
-      spacing={0}
-      direction="column"
-      alignItems="center"
-      justifyContent="center"
-      style={{ minHeight: "95vh" }}
-    >
-      <Box className={styles.boxy} component="form" noValidate>
-        <Typography variant="body1">
-          {loginOrRegister === "login" ? "Login Page" : "Register Page"}
-        </Typography>
+    <Container maxWidth="xs">
+      <CssBaseline/>
+      <Box sx={{ marginTop: 8, display: "flex", flexDirection: "column", alignItems: "center" }} component="form" noValidate>
 
-        <TextField
-          label="Email"
-          type="email"
-          variant="outlined"
-          size="small"
-          value={credential.email}
-          onChange={textFieldEmailOnChangeHandler}
-        />
+      <Avatar sx={{ m: 1, bgcolor: 'secondary.main' }}>
+            <Face />
+          </Avatar>
+        <Typography variant="body1">{loginOrRegister === "login" ? "Sign In" : "Register"}</Typography>
 
-        <TextField
-          label="password"
-          type="Password"
-          variant="outlined"
-          size="small"
-          value={credential.password}
-          onChange={textFieldPasswordOnChangeHandler}
-        />
+        <TextField name="Email" type="email" variant="outlined" margin="normal" required fullWidth label="Email Address" value={credential.email} onChange={textFieldEmailOnChangeHandler} autoComplete="email" />
 
-        <Button
-          variant="outlined"
-          size="small"
-          onClick={buttonLoginOrRegisterOnClickHandler}
-        >
+        <TextField name="Password" type="Password" variant="outlined" margin="normal" required fullWidth label="Password" value={credential.password} onChange={textFieldPasswordOnChangeHandler} autoComplete="current-password" />
+
+        <Button variant="contained" fullWidth sx={{ mt: 3, mb: 2 }} onClick={buttonLoginOrRegisterOnClickHandler}>
           {loginOrRegister === "login" ? "Login" : "Register Account"}
         </Button>
 
         {loginOrRegister === "login" ? (
           <Link to="/register">
-            <Typography variant="body1">or do you want Register ?</Typography>
+            <Typography variant="body1">Or do You Want Register ?</Typography>
           </Link>
         ) : (
           <Link to="/login">
@@ -117,8 +85,7 @@ const LoginOrRegisterForm = ({ loginOrRegister }) => {
           </Link>
         )}
       </Box>
-    </Grid>    
-
+    </Container>
   );
 };
 
